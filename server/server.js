bdc8bd1e20c536d7e94011d328773b4c59cb3a01
Ubuntu <ubuntu@ip-172-31-36-48.us-west-2.compute.internal>
2015-03-30 15:15:20 +0000

    update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.all('*', function(req, res, next) {
 app.get('/flyers', flyers.findAll);
 app.get('/flyers/:id', flyers.findById);
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8080);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
