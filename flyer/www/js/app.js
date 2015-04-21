3bcb5416883df750f2574c51459fa5ece402f19f
dg253 <dg253@njit.edu>
2015-04-20 21:30:03 -0400

    added pouchdb listener. broadcasts changes/deletes

--- flyer/www/js/app.js
+++ flyer/www/js/app.js
@@ -29,6 +29,31 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
+.factory('PouchDBListener', ['$rootScope', function($rootScope) {
+    localDB.changes({
+        continuous: true,
+        onChange: function(change) {
+            if (!change.deleted) {
+                $rootScope.$apply(function() {
+                    localDB.get(change.id, function(err, doc) {
+                        $rootScope.$apply(function() {
+                            if (err) console.log(err);
+                            $rootScope.$broadcast('add', doc);
+                        })
+                    });
+                })
+            } else {
+                $rootScope.$apply(function() {
+                    $rootScope.$broadcast('delete', change.id);
+                });
+            }
+        }
+    });
+
+    return true;
+
+}])
+
 .config(function($stateProvider, $urlRouterProvider) {
   openFB.init({appId: '1630742683822336'});
 
