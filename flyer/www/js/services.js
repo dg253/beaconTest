42e24db5518c3139775eac79c0396c225d0ed052
dg253 <dg253@njit.edu>
2015-05-04 22:17:27 -0400

    -console log each function -updated getFlyer index issue.

--- flyer/www/js/services.js
+++ flyer/www/js/services.js
@@ -1,28 +1,34 @@
 angular.module('starter.services', ['ngResource'])
 
-.service('FlyerService', function (PouchDBListener, $resource) {
+.service('FlyerService', function (PouchDBListener) {
     var flyers1 = [];
 
     var addFlyer = function(flyer) {
-      flyers1.push(flyer);
-      return True
+      console.log('confirmed push')
+      flyers1.push(flyer)
     }
 
     var deleteFlyer = function(id){
       for(var i = 0; i < flyers1.length; i++) {
           if(flyers1[i]._id === id) {
+            console.log('confirmed deletion')
             flyers1.splice(i, 1);
-            return True
           }
       }
     }
 
     var getFlyers = function(){
+      console.log('just returned all flyers')
       return flyers1;
     }
 
-    var getFlyer = function(id){
-      return flyers1[id];
+    var getFlyer = function(flyerId){
+      for(var i = 0; i < flyers1.length; i++) {
+          if(flyers1[i]._id === flyerId) {
+            console.log('just returned a flyer')
+            return flyers1[i];
+          }
+      }
     }
 
     return {
