76b975f4f2808c2f9f75a7fe27b05d78f5c8b08b
dg253 <dg253@njit.edu>
2015-05-06 21:24:17 -0400

    added josh beacon factory

--- flyer/www/js/app.js
+++ flyer/www/js/app.js
@@ -1,5 +1,5 @@
 // Ionic Starter App
-// bruh 
+// bruh
 // fdf
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -51,6 +51,142 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     });
     return true;
 }])
+
+.factory('Beacon', function($rootScope){
+	var beacon = {};
+
+	beacon.beaconRegion = null;
+	beacon.delegate = null;
+
+	beacon.uuid = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6";
+	beacon.identifier = "flyer";
+	beacon.major = 1;
+	beacon.minor = 1;
+
+	//------------------------------------------------------
+
+	beacon.callback = function(){
+		console.log("No callback defined!");
+	};
+
+	beacon.setCallback = function(cb){
+		beacon.callback = cb;
+	};
+
+	//------------------------------------------------------
+
+	beacon.init = function(){
+		beacon.delegate = new cordova.plugins.locationManager.Delegate();
+
+		beacon.delegate.didRangeBeaconsInRegion = function(pluginResult){
+			beacon.didRangeBeaconsInRegion(pluginResult);
+		};
+
+		beacon.delegate.didEnterRegion = function(pluginResult){
+			beacon.didEnterRegion(pluginResult);
+		};
+
+		beacon.delegate.didExitRegion = function(pluginResult){
+			beacon.didExitRegion(pluginResult);
+		};
+
+		beacon.delegate.didDetermineStateForRegion = function(pluginResult){
+			beacon.didDetermineStateForRegion(pluginResult);
+		};
+	};
+
+	beacon.helloWorld = function(){
+		console.log("HELLO WORLD! It\'s me, Beacon Scanner! :)");
+	};
+
+	//------------------------------------------------------
+
+	beacon.startRanging = function(plugin){
+		var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(beacon.identifier,beacon.uuid);
+		cordova.plugins.locationManager.setDelegate(beacon.delegate);
+		//For iOS 8+ punks
+		cordova.plugins.locationManager.requestWhenInUseAuthorization();
+
+		cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
+		  .fail(console.error)
+		  .done();
+
+		  console.log("I\'m looking for: " + beacon.uuid);
+	};
+
+	beacon.stopRanging = function(plugin){
+		var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(beacon.identifier,beacon.uuid,beacon.major,beacon.minor);
+
+		cordova.plugins.locationManager.stopRangingBeaconsInRegion(beaconRegion)
+		  .fail(console.error)
+		  .done();
+
+		  console.log("I stopped ranging for beacons.");
+	};
+
+	//------------------------------------------------------
+
+	beacon.getStatus = function(){
+		return beacon.status;
+	};
+
+	//------------------------------------------------------
+
+	beacon.didEnterRegion = function(plugin){
+		console.log("Hello, from beacon!");
+	};
+
+	beacon.didExitRegion = function(plugin){
+		console.log("Goodbye, from beacon!");
+	};
+
+	beacon.didRangeBeaconsInRegion = function(plugin){
+		console.log(JSON.stringify(plugin));
+		for(b in plugin.beacons){
+			if(beacon.processBeacon(plugin.beacons[b])){
+				console.log("Found it!");
+				beacon.callback(plugin);
+			}
+		}
+	};
+
+	beacon.didDetermineStateForRegion = function(plugin){
+		console.log("Which event was this again...?");
+	};
+
+	//------------------------------------------------------
+
+	beacon.processBeacon = function(b){
+		/*
+		 * We know that most likely we are only searching for a very specific beacon, so just grab
+		 * the first one found with an immediate proximity
+		 */
+
+		console.log("I AM BEACON " + JSON.stringify(b));
+
+		if(b.proximity == "ProximityImmediate"){
+			return true;
+		}
+
+		return false;
+	};
+
+	//------------------------------------------------------
+
+	beacon.setUUID = function(uuid){
+		beacon.uuid = uuid;
+	};
+
+	beacon.setMajor = function(major){
+		beacon.major = major;
+	};
+
+	beacon.setMinor = function(minor){
+		beacon.minor = minor;
+	};
+
+	return beacon;
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
   openFB.init({appId: '1630742683822336'});
