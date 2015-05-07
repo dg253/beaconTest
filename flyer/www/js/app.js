f14d4e75a74af9afaba10c64f0c48570aabba6d1
dg253 <dg253@njit.edu>
2015-05-06 21:25:23 -0400

    added Beacon factory into run func -keyboard accessory bar now shows

--- flyer/www/js/app.js
+++ flyer/www/js/app.js
@@ -11,12 +11,12 @@ var remoteDB = new PouchDB('http://54.149.42.95:5984/beacons', {cache : false});
 
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function(Beacon, $ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
